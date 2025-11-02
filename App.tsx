a26aa80ef91f763b341c288884fe8e1e27931292
PaulaKaa <c3kapa03@students.oamk.fi>
2025-11-02 15:12:30 +0200

    added last requirements

--- App.tsx
+++ App.tsx
@@ -7,21 +7,21 @@ export default function App() {
   //check is it number
   const ageIsNumber: number = !isNaN(Number(age)) === true ? Number(age) : 0;
 
-  const lowHR: number = (200 - ageIsNumber) * 0.65;
-  const highHR: number = (200 - ageIsNumber) * 0.85;
+  const lowHR: number = ageIsNumber ? (200 - ageIsNumber) * 0.65 : 0;
+  const highHR: number = ageIsNumber ? (200 - ageIsNumber) * 0.85 : 0;
 
   return (
     <View style={styles.container}>
       <Text style={styles.heading}>Heart Rate Limits Calculator</Text>
-      <Text>Enter your age:</Text>
+      <Text style={styles.textmargin}>Enter your age:</Text>
       <TextInput
         keyboardType="number-pad"
         value={age}
         onChangeText={setAge}
         style={styles.input}
       ></TextInput>
-      <Text>Lower limit {lowHR.toFixed(2)} bpm</Text>
-      <Text>Upper limit {highHR.toFixed(2)} bpm</Text>
+      <Text style={styles.textmargin}>Lower limit: {lowHR.toFixed(2)} bpm</Text>
+      <Text>Upper limit: {highHR.toFixed(2)} bpm</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -31,14 +31,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    margin: 10,
+    marginTop: 50,
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
   },
   heading: {
-    margin: 8,
     fontWeight: "bold",
     fontSize: 24,
   },
   input: {
-    borderWidth: 0.5 }
+    height: 40,
+    marginTop: 5,
+    borderWidth: 0.5, //border
+    borderRadius: 5,
+    padding: 10,
+    width: 100,
+  },
+  textmargin: {
+    marginTop: 15
+  }
 });
